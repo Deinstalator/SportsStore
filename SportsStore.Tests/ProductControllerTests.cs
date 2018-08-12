5c9001a86dd374f5a3f7e81fcd4b671a8dca8bf6
Deinstalator <radoslaw.antoniak@gmail.com>
2018-08-12 03:17:30 +0200

    Implementacja stronicowania i testów jednostkowych

---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Paginate()
+        {
+            //Przygotownaie
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 1, Name = "P1"},
+            }).AsQueryable<Product>());
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //Działanie
+            IEnumerable<Product> result =
+                controller.List(2).ViewData.Model as IEnumerable<Product>;
+
+            //Asercje
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
+    }
+}
